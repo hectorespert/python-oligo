7ba5d218812dd33bfe9ccfc79f70d4b13168c87e
blackleg <hectorespertpardo@gmail.com>
2017-06-26 16:24:59 +0200

    Add MIT license

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='oligo',
-    version='0.0.1',
+    version='0.0.2',
     packages=['oligo'],
     install_requires=[
           'requests',
       ],
-    url='',
-    license='',
+    url='https://github.com/blackleg/python-oligo',
+    license='MIT',
     author='blackleg',
-    author_email='',
+    author_email='hectorespertpardo@gmail.com',
     description=''
 )
