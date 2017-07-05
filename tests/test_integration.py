a78a536c1dff7f903d792b761cab39823d59ec65
blackleg <hectorespertpardo@gmail.com>
2017-07-05 16:52:30 +0200

    Add login in test integration

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,13 +1,21 @@
 import unittest
 
 from oligo import iber
+from os import environ
 
 
 class TestIberIt(unittest.TestCase):
 
     def test_integration(self):
+        user = environ.get("user", "asdfg")
+        pwd = environ.get("password", "asddfg")
+
         instance = iber.Iber()
         self.assertIsInstance(instance, iber.Iber)
+        instance.login(user, pwd)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
